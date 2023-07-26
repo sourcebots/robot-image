faebb97101cf6dcc6899ec331159fe6453af74da
WillB97 <will@rudi.org.uk>
2023-07-26 22:48:39 +0100

    Fix permissions with udiskie

--- parts/robot.sh
+++ parts/robot.sh
@@ -14,6 +14,22 @@ apt-get install -y $PACKAGES_DIR/*.deb
 apt-get install -y udiskie
 cp /tmp/packer-files/udiskie/udiskie.yml /etc/
 cp /tmp/packer-files/udiskie/udiskie.service /lib/systemd/system/
+systemctl enable udiskie.service
+
+# Configure udisks2 to allow mounting as other users
+mkdir -p /etc/udisks2
+cat >/etc/udisks2/mount_options.conf <<EOF
+### Simple global overrides
+[defaults]
+# common options, applied to any filesystem, always merged with specific filesystem type options
+allow=uid=$(id -u robot),uid=$(id -u runusb),gid=$(id -g robot),exec,noexec,nodev,nosuid,atime,noatime,nodiratime,ro,rw,sync,dirsync,noload
+EOF
+
+# Automatically mount USB drives as the runusb user with the robot group
+echo "  - options: [uid=$(id -u runusb), gid=$(id -g robot)]" >> /etc/udiskie.yml
+
+# Make the base mount point accessible to all users
+mkdir -p --mode=777 /media/root
 
 # Install core components
 pip install --no-cache -r /tmp/packer-files/requirements.txt
